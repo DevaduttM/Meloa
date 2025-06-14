10304ede8592885433a788df7153c2b94978333e
Devadutt <devaduttmohancs@gmail.com>
2025-06-14 14:00:41 +0530

    Shimmer loading animation added

---
+++ src/components/HomeScreenShimmer.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import Image from 'next/image'
+import { FaRegCircleUser } from "react-icons/fa6";
+import { IoMdMore } from 'react-icons/io'
+import { motion } from 'framer-motion'
+
+
+
+const HomeScreenShimmer = () => {
+  return (
+    <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          className="h-screen w-screen flex justify-start items-center bg-[#171717] flex-col relative overflow-x-hidden overflow-y-scroll scrollbar-hide"
+        >
+          <div className="top-0 w-full flex justify-between px-3 pt-7 items-center">
+            <div className="w-fit h-full flex justify-center items-center gap-2">
+              <Image
+                src="/logo_img_only.png"
+                alt="Logo"
+                width={35}
+                height={35}
+              />
+              <h1 className="text-transparent bg-gradient-to-r from-[#27df6a] to-[#afafaf] bg-clip-text text-3xl font-bold font-syne">
+                Meloa
+              </h1>
+            </div>
+            <div className="w-fit h-full flex justify-center items-center gap-2">
+              <FaRegCircleUser className="text-3xl text-[#27df6a] mr-2" />
+            </div>
+          </div>
+          <div className="w-full flex flex-col justify-center items-start mt-10">
+            <div className="bg-[#1f1f1f] ml-6 h-7 w-[35%] rounded-md animate-pulse"/>
+            <div className="w-full pl-6 py-8 flex flex-row justify-start items-center overflow-x-scroll scrollbar-hide">
+              {[...Array(2)].map((_, outerIndex) => (
+                <div
+                  key={outerIndex}
+                  className="min-w-[65vw] flex-col gap-5 h-full flex justify-between items-center mr-4"
+                >
+                  {[...Array(3)].map((_, index) => (
+                    <div
+                      key={index}
+                      className="w-full h-full bg-[#1f1f1f] rounded-md animate-pulse flex justify-between items-center mb-3"
+                    >
+                      <div className="flex p-[6px] justify-center items-center gap-3 w-full">
+                        <div className="h-14 w-18 bg-[#222222] rounded-md"/>
+                        <div className="h-full w-full flex flex-col gap-2 items-start justify-center">
+                          <div className="h-5 w-40 bg-[#222222] rounded-md"/>
+                          <div className="h-3 w-20 bg-[#222222] rounded-md"/>
+                        </div>
+                      </div>
+                      <div className="h-6 w-2 bg-[#222222] rounded-md mr-3"/>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+
+            <div className="bg-[#1f1f1f] ml-6 h-7 w-[35%] rounded-md animate-pulse"/>
+            <div className="relative w-full grid grid-cols-2 grid-rows-3 gap-4 pl-3 pr-5 mt-8">
+              {[...Array(6)].map((_, index) => (
+                <div
+                  key={index}
+                  className="relative bg-[#1f1f1f] animate-pulse rounded-md h-25 w-full m-1 flex justify-center items-center overflow-hidden"
+                >
+                    <div className="h-5 w-30 bg-[#222222] rounded-md animate-pulse"/>
+                </div>
+              ))}
+            </div>
+
+            <div className="bg-[#1f1f1f] ml-6 h-7 w-[50%] mt-10 rounded-md animate-pulse"/>
+            <div className="w-full pl-6 py-8 flex flex-row justify-start items-center overflow-x-scroll scrollbar-hide pb-30">
+              {[...Array(2)].map((_, outerIndex) => (
+                <div
+                  key={outerIndex}
+                  className="min-w-[65vw] flex-col gap-5 h-full flex justify-between items-center mr-4"
+                >
+                  {[...Array(3)].map((_, index) => (
+                    <div
+                      key={index}
+                      className="w-full h-full bg-[#1f1f1f] animate-pulse rounded-md flex justify-between items-center mb-3"
+                    >
+                      <div className="flex p-[6px] justify-center items-center gap-3 w-full">
+                        <div className="h-14 w-18 bg-[#222222] rounded-md"/>
+                        <div className="h-full w-full flex flex-col gap-2 items-start justify-center">
+                          <div className="h-5 w-40 bg-[#222222] rounded-md"/>
+                          <div className="h-3 w-20 bg-[#222222] rounded-md"/>
+                        </div>
+                      </div>
+                      <div className="h-6 w-2 bg-[#222222] rounded-md mr-3"/>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </motion.div>
+  )
+}
+
+export default HomeScreenShimmer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,35 @@
 "use client"
-import React, { useState } from 'react'
-import { AiFillHome } from "react-icons/ai";
-import { RiSearch2Line } from "react-icons/ri";
-import { MdVideoLibrary } from "react-icons/md";
+import React, { useEffect, useState } from 'react'
+import { AiFillHome, AiOutlineHome } from "react-icons/ai";
+import { RiSearch2Line, RiSearch2Fill } from "react-icons/ri";
+import { MdVideoLibrary, MdOutlineVideoLibrary } from "react-icons/md";
 import HomeScreen from './HomeScreen';
 import SearchScreen from './SearchScreen';
 import LibraryScreen from './LibraryScreen';
+import { motion } from 'framer-motion';
+import HomeScreenShimmer from './HomeScreenShimmer';
 
 const Navbar = () => {
 
     const [page, setPage] = useState("home");
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
 
   return (
     <>
         <div className="h-screen w-screen flex justify-center items-center bg-[#171717]">
             {
-                page === "home" ? (
-                    <HomeScreen />
+                page === "home" && loading ? (
+                    <HomeScreenShimmer />
+                ) : page === "home" ? (
+                    <HomeScreen setPage={setPage} />
                 ) : page === "search" ? (
                     <SearchScreen />
                 ) : page === "library" ? (
@@ -25,16 +38,37 @@ const Navbar = () => {
             }
             <div className="fixed bottom-0 w-full h-[7%] bg-gradient-to-t from-black to-[#00000052] backdrop-blur-lg flex items-center justify-around px-10">
                 <div onClick={() => setPage("home")} className="flex h-full items-center justify-center flex-col">
-                    <AiFillHome className= {`text-xl ${page === "home" ? "text-[#27df6a]" : "white"} transition-colors duration-200`} />
-                    <span className={`font-syne text-xs ${page === "home" ? "text-[#27df6a]" : "white"}`}>Home</span>
+                    {
+                        page === "home" ? (
+                            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                            <AiFillHome className= {`text-xl ${page === "home" ? "text-[#27df6a]" : "white"}`} /></motion.div>
+                        ) : 
+                        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                        <AiOutlineHome className= {`text-xl text-white`} /></motion.div>
+                    }
+                    <span className={`font-syne transition-colors duration-200 text-xs ${page === "home" ? "text-[#27df6a]" : "white"}`}>Home</span>
                 </div>
                 <div onClick={() => setPage("search")} className="flex h-full items-center justify-center flex-col">
-                    <RiSearch2Line className= {`text-xl ${page === "search" ? "text-[#27df6a]" : "white"} transition-colors duration-200`}  />
-                    <span className={`font-syne text-xs ${page === "search" ? "text-[#27df6a]" : "white"}`}>Search</span>
+                    {
+                        page === "search" ? (
+                            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                            <RiSearch2Fill className= {`text-xl ${page === "search" ? "text-[#27df6a]" : "white"} `} /></motion.div>
+                        ) : 
+                        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                        <RiSearch2Line className= {`text-xl text-white`} /></motion.div>
+                    }
+                    <span className={`font-syne transition-colors duration-200 text-xs ${page === "search" ? "text-[#27df6a]" : "white"}`}>Search</span>
                 </div>
                 <div onClick={() => setPage("library")} className="flex h-full items-center justify-center flex-col">
-                    <MdVideoLibrary className= {`text-xl ${page === "library" ? "text-[#27df6a]" : "white"} transition-colors duration-200`} />
-                    <span className={`font-syne text-xs ${page === "library" ? "text-[#27df6a]" : "white"}`}>Library</span>
+                    {
+                        page === "library" ? (
+                            <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                            <MdVideoLibrary className= {`text-xl ${page === "library" ? "text-[#27df6a]" : "white"}`} /></motion.div>
+                        ) : 
+                        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.2 }}>
+                        <MdOutlineVideoLibrary className= {`text-xl text-white`} /></motion.div>
+                    }
+                    <span className={`font-syne transition-colors duration-200 text-xs ${page === "library" ? "text-[#27df6a]" : "white"}`}>Library</span>
                 </div>
             </div>
         </div>
